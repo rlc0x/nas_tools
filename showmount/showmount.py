b96ee808b0674d91550a5831954954754f2d0f22
rlc0x <rcox@lunartix.com>
2017-06-07 15:25:32 -0600

    Updating showmount to make it more generic

--- showmount/showmount.py
+++ showmount/showmount.py
@@ -12,24 +12,31 @@ class ShowMount(object):
         if nas is not None:
             self.nas = nas
         else:
-            raise ValueError('Name of the nas is required')
+            raise ValueError('Name or IP address of the nas is required')
+
+        self.re_nfs_alias = re.compile('(?P<nfs_alias>(/\w+))$')
+        self.re_nas_clients = re.compile('(?P<nfs_client>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|(\w+)?([\.|\w]+)):(?P<vfs_client>(.\w+?([\/|\w]+)))')
+        self.nas_clients = defaultdict(lambda: defaultdict(list))
+        self.nas_clients_cmd = 'showmount --no-headers -a {}'.format(self.nas)
+        self.vfs_dirs_cmd = 'showmount --no-headers -e {}'.format(self.nas)
 
     def get_vfs_dirs(self):
-        vfs_dirs = []
-        vfs_dirs_cmd = 'showmount --no-headers -e {}'.format(self.nas)
-        for line in subprocess.Popen(vfs_dirs_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8').split('\n'):
+        vfs_dirs = set()
+        for line in subprocess.Popen(self.vfs_dirs_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8').split('\n'):
             line = line.strip().split(' ')
-            if re.match(r'/root_vdm.*', line[0]):
-                vfs_dirs.append(line[0])
-        return vfs_dirs 
-            
+            '''
+            The format returned could be either the full path to the share, or just the alias
+            '''
+            re_nfs_alias_match = self.re_nfs_alias.search(line[0])
+            if re_nfs_alias_match:
+                nfs_alias = re_nfs_alias_match.group('nfs_alias')
+                vfs_dirs.add(nfs_alias)
+        return vfs_dirs
+
     def get_nas_clients(self):
-        nas_clients = defaultdict(lambda: defaultdict(list))
-        nas_clients_cmd = 'showmount --no-headers -a {}'.format(self.nas)
-        re_nas_clients = re.compile('(?P<nfs_client>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|(\w+)?([\.|\w]+)):(?P<vfs_client>(.\w+?([\/|\w]+)))')
-        for line in subprocess.Popen(nas_clients_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8').split('\n'):
+        for line in subprocess.Popen(self.nas_clients_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8').split('\n'):
             try:
-                re_nas_clients_match = re_nas_clients.search(line)
+                re_nas_clients_match = self.re_nas_clients.search(line)
                 if re_nas_clients_match:
                     client_name = re_nas_clients_match.group('nfs_client')
                     vfs = re_nas_clients_match.group('vfs_client')
@@ -52,6 +59,6 @@ def getargs():
 if __name__ == '__main__':
     args = getargs()
     SM = ShowMount(nas=args.nas)
-    nas_client_list = SM.get_nas_clients()
+    nas_client_list = SM.get_vfs_dirs()
     if nas_client_list:
-        print(json.dumps(nas_client_list, indent=4))
+        print(json.dumps(str(nas_client_list), indent=4))
